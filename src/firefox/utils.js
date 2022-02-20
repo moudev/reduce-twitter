aaa9083f10a2a8d699b0182ccd94dd4237642280
moudev <rigobertomauriciomartinez@gmail.com>
2022-02-19 21:09:00 -0600

    feat: add update css when actions change

--- src/firefox/utils.js
+++ src/firefox/utils.js
@@ -1,5 +1,7 @@
 import defaultActions from "./data.js"
 
+const actions = defaultActions
+
 function getTabs() {
   return browser.tabs.query({
     active: true,
@@ -7,10 +9,36 @@ function getTabs() {
   })
 }
 
+async function updateCSS () {
+  const tabs = await getTabs()
+  
+  if (tabs.length === 0) return
+  
+  const tab = tabs[0] // current active twitter tab
+
+  let css = ""
+  
+  actions.map(action => {
+    const selectors = action.selectors.join(", ")
+
+    const declarations = action.rules
+      .map(rule => rule.apply ? rule.value : rule.default)
+      .join(" ")
+    
+    const rule = `${selectors} {
+      ${declarations}
+    }`    
+
+    css += rule    
+  })
+
+  // if the css isn't removed the css update action only works one time
+  browser.tabs.removeCSS(tab.id, { code: css }); 
+  browser.tabs.insertCSS(tab.id, { code: css }); 
+}
+
 function updateAction(e) {
   if (e.target.type !== "checkbox") return
-
-  const actions = defaultActions
 
   const checked = e.target.checked
   const [action, rule] = e.target.id.split("-")
@@ -19,6 +47,8 @@ function updateAction(e) {
   const ruleIndex = actions[actionIndex].rules.findIndex(r => r.id === rule)
 
   actions[actionIndex].rules[ruleIndex].apply = checked
+
+  updateCSS()
 }
 
 export { updateAction, getTabs }
